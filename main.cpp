a0af730c4054e991903da018ca0b4262d08bd0aa
poallele <249226774+poallele@users.noreply.github.com>
2026-09-18 13:44:55 -0400

    Refactor Measurement and Isotopes structs

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
+// CPPRadioactiveDecayCalc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <cmath>
 #include <iomanip>
+using namespace std;
 
-struct Measurement  {
-                        std::string character;
-                        std::string unit;
-                        int multiplier;
-                    };
-struct Isotopes {
-                    std::string name;
-                    long double value;
-                };
+struct Measurement
+{
+	string character;
+	string unit;
+	int multiplier;
+};
+
+struct Isotopes
+{
+	string name;
+	long double value;
+};
 
 int main()
 {
@@ -20,22 +27,23 @@ int main()
 	 * the matching asterisks to the left
 	 * can make this easier to read
 	 */
-    
-    /* if they choose 'seconds' then the half-lives (in the form of years,
-     * scientific notation) are converted into seconds by multiplying
-	 * them by the number of seconds in the gregorian calendar.
-	 * dividing them by 3600 (amount of seconds in an hour)
-	 * yields hours. */
-    const int GREGORIAN_SECONDS = 31556952;
-    std::vector<Measurement> measurement =  {
-                                                {"s","second",GREGORIAN_SECONDS/1},
-                                                {"m","minute",GREGORIAN_SECONDS/60},
-                                                {"h","hour",GREGORIAN_SECONDS/3600},
-                                                {"d","day",GREGORIAN_SECONDS/86400},
-                                                {"y","year",GREGORIAN_SECONDS/GREGORIAN_SECONDS}
-                                            };
-	
-	std::string errorMessage = "Invalid character. ";
+
+	 /* if they choose 'seconds' then the half-lives (in the form of years,
+	  * scientific notation) are converted into seconds by multiplying
+	  * them by the number of seconds in the gregorian calendar.
+	  * dividing them by 3600 (amount of seconds in an hour)
+	  * yields hours. */
+	const int GREGORIAN_SECONDS = 31556952;
+	vector<Measurement> measurement =
+	{
+		{"s","second",GREGORIAN_SECONDS / 1},
+		{"m","minute",GREGORIAN_SECONDS / 60},
+		{"h","hour",GREGORIAN_SECONDS / 3600},
+		{"d","day",GREGORIAN_SECONDS / 86400},
+		{"y","year",GREGORIAN_SECONDS / GREGORIAN_SECONDS}
+	};
+
+	string errorMessage = "Invalid character. ";
 
 	/* user prompt. 'print' = (std::cout <<). 'submit' = (std::cin >>).
 	 * exitLoop1 is initialized as false. while (exitLoop1)
@@ -44,17 +52,26 @@ int main()
 	 * "if-else" conditionals are utilized instead of "switch" statements
 	 * for their ability to use strings and logical comparisons. */
 	bool exitLoop1 = false;
-	std::string response1{};
+	string response1{};
 	int responseIndex1{};
-    int count1{};
-    std::cout << "Would you like to measure in:\n";
-    for (const auto & m:measurement) {
-        std::cout << "<" << measurement[count1].character << "> " << measurement[count1].unit << "s\n";
-        count1 += 1;
-    }
-	while (!exitLoop1) {
-		std::cin >> response1;
-    	if (response1 == measurement[0].character) {
+	int count1{};
+	cout << "Would you like to measure in:\n";
+	
+	for (const auto& m : measurement)
+	{
+		cout
+			<< "<"
+			<< measurement[count1].character
+			<< "> "
+			<< measurement[count1].unit
+			<< "s\n";
+		count1 += 1;
+	}
+
+	while (!exitLoop1)
+	{
+		cin >> response1;
+		if (response1 == measurement[0].character) {
 			responseIndex1 = 0;
 			exitLoop1 = true;
 		}
@@ -75,30 +92,30 @@ int main()
 			exitLoop1 = true;
 		}
 		else {
-			std::cout << errorMessage;
+			cout << errorMessage;
 		}
 	}
-	
-	std::vector<Isotopes> isotopes =
-	    {
-	        {"Uranium-233",1.592e5*measurement[responseIndex1].multiplier},
-	        {"Uranium-235",7.04e8*measurement[responseIndex1].multiplier},
-	        {"Uranium-238",4.463e9*measurement[responseIndex1].multiplier},
-	        {"Plutonium-239",2.411e4*measurement[responseIndex1].multiplier},
-	        {"Thorium-232",1.40e10*measurement[responseIndex1].multiplier}
-	    };
-    
-    // user prompt
-    bool exitLoop2 = false;
+
+	vector<Isotopes> isotopes =
+	{
+		{"Uranium-233",1.592e5 * measurement[responseIndex1].multiplier},
+		{"Uranium-235",7.04e8 * measurement[responseIndex1].multiplier},
+		{"Uranium-238",4.463e9 * measurement[responseIndex1].multiplier},
+		{"Plutonium-239",2.411e4 * measurement[responseIndex1].multiplier},
+		{"Thorium-232",1.40e10 * measurement[responseIndex1].multiplier}
+	};
+
+	// user prompt
+	bool exitLoop2 = false;
 	int response2{};
 	int count2 = 0;
-	std::cout << "Select a fertile radioactive isotope:\n";
-	for (const auto & i:isotopes) {
-        count2 += 1;
-        std::cout << "<" << count2 << "> " << i.name << '\n';
-    }
-    while (!exitLoop2) {
-		std::cin >> response2;
+	cout << "Select a fertile radioactive isotope:\n";
+	for (const auto& i : isotopes) {
+		count2 += 1;
+		cout << "<" << count2 << "> " << i.name << '\n';
+	}
+	while (!exitLoop2) {
+		cin >> response2;
 		if (response2 == 1) {
 			exitLoop2 = true;
 		}
@@ -115,21 +132,21 @@ int main()
 			exitLoop2 = true;
 		}
 		else {
-			std::cout << errorMessage;
+			cout << errorMessage;
 		}
 	}
 	/* since the index "[]" for a vector begins at 0, "-1" is used
 	 * (since user responses begin at 1). if this was not used,
-	 * then response2 would equal 6 instead of 5. since 6 is not 
+	 * then response2 would equal 6 instead of 5. since 6 is not
 	 * present in the index, it would not return anything. */
-	int responseIndex2{response2-1};
-	
+	int responseIndex2{ response2 - 1 };
+
 	/* decay constant | the natural logarithm of  2 divided by the half life.
 	 * print the decay constant as a number with 20 decimal places. */
-	const double decay = (std::log(2)) / isotopes[responseIndex2].value;
-	std::cout
-	    << "Probability per " << measurement[responseIndex1].unit << " for a single " << isotopes[response2-1].name
-	    << " nucleus to decay: " << std::fixed << std::setprecision(25) << decay;
-	
+	const double decay = (log(2)) / isotopes[responseIndex2].value;
+	cout
+		<< "Probability per " << measurement[responseIndex1].unit << " for a single " << isotopes[response2 - 1].name
+		<< " nucleus to decay: " << std::fixed << std::setprecision(25) << decay;
+
 	return 0;
 }
