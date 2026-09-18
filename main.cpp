cb0310dc96e423edcae0035c2657141bb47d510b
poallele <249226774+poallele@users.noreply.github.com>
2026-09-18 14:15:20 -0400

    Improve input validation and measurement structure
    Refactor user input handling and measurement initialization.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include <iomanip>
 using namespace std;
 
+
+
+//=============================================================================================
+// Task
+//=============================================================================================
 struct Measurement
 {
 	string character;
@@ -21,12 +26,28 @@ struct Isotopes
 	long double value;
 };
 
+
+
 int main()
 {
 	//=============================================================================================
 	// Task
 	//=============================================================================================
+	
+	
+	
 	const int GREGORIAN_SECONDS = 31556952;
+	string errorMessage = "Invalid character. ";
+	
+	bool exitLoop1 = false;
+	string response1{};
+	int responseIndex1{};
+	int count1{};
+
+	bool exitLoop2 = false;
+	int response2{};
+	int count2 = 0;
+
 	vector<Measurement> measurement =
 	{
 		{"s","second",GREGORIAN_SECONDS / 1},
@@ -35,10 +56,13 @@ int main()
 		{"d","day",GREGORIAN_SECONDS / 86400},
 		{"y","year",GREGORIAN_SECONDS / GREGORIAN_SECONDS}
 	};
+	
+	
+	
 	//=============================================================================================
 	// Task
 	//=============================================================================================
-	string errorMessage = "Invalid character. ";
+	
 
 	/* user prompt. 'print' = (std::cout <<). 'submit' = (std::cin >>).
 	 * exitLoop1 is initialized as false. while (exitLoop1)
@@ -46,10 +70,7 @@ int main()
 	 * exitLoop1 only becomes "true" by entering valid characters.
 	 * "if-else" conditionals are utilized instead of "switch" statements
 	 * for their ability to use strings and logical comparisons. */
-	bool exitLoop1 = false;
-	string response1{};
-	int responseIndex1{};
-	int count1{};
+	
 	cout << "Would you like to measure in:\n";
 
 	for (const auto& m : measurement)
@@ -62,39 +83,63 @@ int main()
 			<< "s\n";
 		count1 += 1;
 	}
+	
+	
+	
 	//=============================================================================================
 	// Task
 	//=============================================================================================
+	
+	
+	
 	while (!exitLoop1)
 	{
 		cin >> response1;
-		if (response1 == measurement[0].character) {
+		
+		if (response1 == measurement[0].character)
+		{
 			responseIndex1 = 0;
 			exitLoop1 = true;
 		}
-		else if (response1 == measurement[1].character) {
+		
+		else if (response1 == measurement[1].character)
+		{
 			responseIndex1 = 1;
 			exitLoop1 = true;
 		}
-		else if (response1 == measurement[2].character) {
+		
+		else if (response1 == measurement[2].character)
+		{
 			responseIndex1 = 2;
 			exitLoop1 = true;
 		}
-		else if (response1 == measurement[3].character) {
+		
+		else if (response1 == measurement[3].character)
+		{
 			responseIndex1 = 3;
 			exitLoop1 = true;
 		}
-		else if (response1 == measurement[4].character) {
+		
+		else if (response1 == measurement[4].character)
+		{
 			responseIndex1 = 4;
 			exitLoop1 = true;
 		}
-		else {
+		
+		else
+		{
 			cout << errorMessage;
 		}
 	}
+	
+	
+	
 	//=============================================================================================
 	// Task
 	//=============================================================================================
+	
+	
+	
 	vector<Isotopes> isotopes =
 	{
 		{"Uranium-233",1.592e5 * measurement[responseIndex1].multiplier},
@@ -103,47 +148,78 @@ int main()
 		{"Plutonium-239",2.411e4 * measurement[responseIndex1].multiplier},
 		{"Thorium-232",1.40e10 * measurement[responseIndex1].multiplier}
 	};
+	
+	
+	
 	//=============================================================================================
 	// Task
 	//=============================================================================================
+	
+	
+	
 	// user prompt
-	bool exitLoop2 = false;
-	int response2{};
-	int count2 = 0;
 	cout << "Select a fertile radioactive isotope:\n";
 	for (const auto& i : isotopes)
 	{
 		count2 += 1;
-		cout << "<" << count2 << "> " << i.name << '\n';
+		cout
+			<< "<"
+			<< count2
+			<< "> "
+			<< i.name
+			<< '\n';
 	}
+	
+	
+	
 	//=============================================================================================
 	// Task
 	//=============================================================================================
+	
+	
+	
 	while (!exitLoop2)
 	{
 		cin >> response2;
-		if (response2 == 1) {
+		
+		if (response2 == 1)
+		{
 			exitLoop2 = true;
 		}
-		else if (response2 == 2) {
+		
+		else if (response2 == 2)
+		{
 			exitLoop2 = true;
 		}
-		else if (response2 == 3) {
+		
+		else if (response2 == 3)
+		{
 			exitLoop2 = true;
 		}
-		else if (response2 == 4) {
+		
+		else if (response2 == 4)
+		{
 			exitLoop2 = true;
 		}
-		else if (response2 == 5) {
+		
+		else if (response2 == 5)
+		{
 			exitLoop2 = true;
 		}
+		
 		else {
 			cout << errorMessage;
 		}
 	}
+	
+	
+	
 	//=============================================================================================
 	// Task
 	//=============================================================================================
+	
+	
+	
 	/* since the index "[]" for a vector begins at 0, "-1" is used
 	 * (since user responses begin at 1). if this was not used,
 	 * then response2 would equal 6 instead of 5. since 6 is not
@@ -162,5 +238,7 @@ int main()
 		<< std::setprecision(25)
 		<< decay;
 
+	
+	
 	return 0;
 }
