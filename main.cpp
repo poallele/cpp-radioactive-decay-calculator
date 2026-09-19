32a2835aba7324aec62bdf5b506039f580170e5f
poallele <249226774+poallele@users.noreply.github.com>
2026-09-18 23:09:54 -0400

    FIXED PROMPT2 GLITCH AND REFUSES NON-INTS FOR PROMPT 2 NOW

--- main.cpp
+++ main.cpp
@@ -34,8 +34,8 @@ int main()
 	//=============================================================================================
 	// Task
 	//=============================================================================================
-	const long double	GREGORIAN_CALENDAR_SECONDS	=	31556952;
-	const string		ERROR_MESSAGE				=	"Invalid character. ";
+	const long double	GREGORIAN_CALENDAR_SECONDS = 31556952;
+	const string		ERROR_MESSAGE = "Invalid character. ";
 
 	vector<TimespanStruct>TimespanVector =
 	{
@@ -45,7 +45,7 @@ int main()
 		{"d",	"day",		86400},
 		{"y",	"year",		GREGORIAN_CALENDAR_SECONDS}
 	};
-	
+
 	vector<IsotopeStruct>IsotopeVector =
 	{
 		{"Uranium-233",		1.592e5},
@@ -54,9 +54,9 @@ int main()
 		{"Plutonium-239",	2.411e4},
 		{"Thorium-232",		1.405e10}
 	};
-	
-	
-	
+
+
+
 	//=============================================================================================
 	// Task
 	//=============================================================================================
@@ -64,15 +64,15 @@ int main()
 	int					indexOfChosenMeasurementUnitsInVector{};
 	bool				exitInputValidationLoopForMeasurementUnits = false;
 
-	int					chosenIsotopeResponse;
+	double				chosenIsotopeResponse;
 	int					indexOfChosenIsotopeInVector{};
 	bool				exitInputValidationLoopForIsotopes = false;
 
 	int					numberedOrderOfIsotopes = 0;
 	long double			decayConstant{};
 
-	
-	
+
+
 	//=============================================================================================
 	// Prompt 1
 	//=============================================================================================
@@ -80,21 +80,21 @@ int main()
 		<< "Select a timespan for measurement:"
 		<< endl;
 
-	for (const auto& currentTimespan:TimespanVector)
+	for (const auto& currentTimespan : TimespanVector)
 	{
 		cout
 			<< '<'
 			<< currentTimespan.firstCharacter
 			<< '>'
 			<< ' '
-			
+
 			<< currentTimespan.unit
 			<< 's'
 			<< endl;
 	}
-	
-	
-	
+
+
+
 	//=============================================================================================
 	// Input Validation 1
 	//=============================================================================================
@@ -103,53 +103,53 @@ int main()
 										// "while (exitInputValidationLoopForMeasurementUnits)" means "while exitInputValidationLoopForMeasurementUnits is true..."
 										// The (!) makes it negative.
 										// exitInputValidationLoopForMeasurementUnits only becomes "true" by entering valid characters.
-		
+
 		cin >> chosenMeasurementUnitResponse;
-		
+
 		if												// 'If-else' conditionals are utilized instead of 'switch' statements
 														// for their ability to use strings and logical comparisons.
 			(chosenMeasurementUnitResponse == TimespanVector[0].firstCharacter)
-				{
-					indexOfChosenMeasurementUnitsInVector = 0;
-					exitInputValidationLoopForMeasurementUnits = true;
-				}
-		
+		{
+			indexOfChosenMeasurementUnitsInVector = 0;
+			exitInputValidationLoopForMeasurementUnits = true;
+		}
+
 		else if
 			(chosenMeasurementUnitResponse == TimespanVector[1].firstCharacter)
-				{
-					indexOfChosenMeasurementUnitsInVector = 1;
-					exitInputValidationLoopForMeasurementUnits = true;
-				}
-		
+		{
+			indexOfChosenMeasurementUnitsInVector = 1;
+			exitInputValidationLoopForMeasurementUnits = true;
+		}
+
 		else if
 			(chosenMeasurementUnitResponse == TimespanVector[2].firstCharacter)
-				{
-					indexOfChosenMeasurementUnitsInVector = 2;
-					exitInputValidationLoopForMeasurementUnits = true;
-				}
-		
+		{
+			indexOfChosenMeasurementUnitsInVector = 2;
+			exitInputValidationLoopForMeasurementUnits = true;
+		}
+
 		else if
 			(chosenMeasurementUnitResponse == TimespanVector[3].firstCharacter)
-				{
-					indexOfChosenMeasurementUnitsInVector = 3;
-					exitInputValidationLoopForMeasurementUnits = true;
-				}
-		
+		{
+			indexOfChosenMeasurementUnitsInVector = 3;
+			exitInputValidationLoopForMeasurementUnits = true;
+		}
+
 		else if
 			(chosenMeasurementUnitResponse == TimespanVector[4].firstCharacter)
-				{
-					indexOfChosenMeasurementUnitsInVector = 4;
-					exitInputValidationLoopForMeasurementUnits = true;
-				}
-		
+		{
+			indexOfChosenMeasurementUnitsInVector = 4;
+			exitInputValidationLoopForMeasurementUnits = true;
+		}
+
 		else
-			{
-				cout << ERROR_MESSAGE;
-			}
+		{
+			cout << ERROR_MESSAGE;
+		}
 	}
-	
-	
-	
+
+
+
 	//=============================================================================================
 	// Prompt 2
 	//=============================================================================================
@@ -157,84 +157,72 @@ int main()
 		<< endl
 		<< "Select a radioactive isotope:"
 		<< endl;
-	
-	for (const auto & currentIsotope:IsotopeVector)
+
+	for (const auto& currentIsotope : IsotopeVector)
 	{
 		numberedOrderOfIsotopes = numberedOrderOfIsotopes + 1;
-		
+
 		cout
 			<< '<'
 			<< numberedOrderOfIsotopes
 			<< '>'
 			<< ' '
-			
+
 			<< currentIsotope.name
 			<< endl;
 	}
-	
-	
-	
+
+
+
 	//=============================================================================================
 	// Input Validation 2
 	//=============================================================================================
 	while (!exitInputValidationLoopForIsotopes)
 	{
 		cin >> chosenIsotopeResponse;
-		
-		if (chosenIsotopeResponse == 1)
-			{
-				exitInputValidationLoopForIsotopes = true;
-			}
-		
-		else if (chosenIsotopeResponse == 2)
-			{
-				exitInputValidationLoopForIsotopes = true;
-			}
-		
-		else if (chosenIsotopeResponse == 3)
-			{
-				exitInputValidationLoopForIsotopes = true;
-			}
-		
-		else if (chosenIsotopeResponse == 4)
-			{
-				exitInputValidationLoopForIsotopes = true;
-			}
-		
-		else if (chosenIsotopeResponse == 5)
-			{
-				exitInputValidationLoopForIsotopes = true;
-			}
-		
+
+		if
+		(
+			chosenIsotopeResponse == int(chosenIsotopeResponse / 1)
+			&&
+			chosenIsotopeResponse >= 1
+			&&
+			chosenIsotopeResponse <= 5
+			
+		)
+		{
+			exitInputValidationLoopForIsotopes = true;
+		}
+
 		else
-			{
-				cout << ERROR_MESSAGE;
-			}
+		{
+			cout << ERROR_MESSAGE;
+		}
 	}
-	
-	
-	
+
+
+
 	//=============================================================================================
 	// Print final output
 	//=============================================================================================
-	
+
 	/* since the index "[]" for a vector begins at 0, "-1" is used
 	 * (since user responses begin at 1). if this was not used,
 	 * then chosenIsotopeResponse would equal 6 instead of 5. since 6 is not
 	 * present in the index, it would not return anything. */
-	
-	indexOfChosenIsotopeInVector = chosenIsotopeResponse-1;
+
+	indexOfChosenIsotopeInVector = chosenIsotopeResponse - 1;
 
 	decayConstant =	// Decay constant is the (natural logarithm of 2) divided by the half life. It can be multiplied by the amount of units
-		
+
 		(log(2) /
-		IsotopeVector[indexOfChosenIsotopeInVector].halfLife)
-		
+			IsotopeVector[indexOfChosenIsotopeInVector].halfLife)
+
 		*
 
 		(GREGORIAN_CALENDAR_SECONDS /
-		TimespanVector[indexOfChosenMeasurementUnitsInVector].secondsInASingularUnit);
-	
+			TimespanVector[indexOfChosenMeasurementUnitsInVector].secondsInASingularUnit);
+
 
 	cout
 		<< endl
@@ -255,7 +243,7 @@ int main()
 		<< endl
 		<< endl;
 
-	
-	
+
+
 	return 0;
 }
