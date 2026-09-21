85523c42e421f9b0c3ac858098a485c4a2457107
poallele <249226774+poallele@users.noreply.github.com>
2026-09-21 14:53:32 -0400

    Update reference comment in main.cpp

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main()
 	const long double	GREGORIAN_CALENDAR_SECONDS = 31556952;
 	const string		ERROR_MESSAGE = "Invalid character. ";
 
-	// https://manara.edu.sy/downloads/files/1681300155_Refrence.pdf (PAGE 714 WILL ADD LATER) (emplace_back PAGE 1083)
+	// https://manara.edu.sy/downloads/files/1681300155_Refrence.pdf (PAGE 714 WILL ADD LATER) (emplace_back PAGE 1083) (for loops PAGE 286)
 
 	int AMOUNT_OF_MEASUREMENTS				= 5;	// default amount
 	AMOUNT_OF_MEASUREMENTS					+= 1;	// How many more measurements do you want to add?
